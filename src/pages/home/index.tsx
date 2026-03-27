222adab7e72d1fee30bf6c936d21092060a77684
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-03-27 16:06:46 -0300

    Estrutura inicial do projeto

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+import Header from '../../components/header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Button>Clique-me</Button>
+      Home
+    </div>
+  );
+};
+export default Home;
