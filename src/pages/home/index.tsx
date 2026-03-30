3c1ed4df4d46c17cf16fd64575418321d3fc1f46
Alysson Wanderley <105825366+alyssonwa@users.noreply.github.com>
2026-03-30 11:36:09 -0300

    adicionando hooks e manipulação de estados para a listagem de pokemons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,46 @@
 import { Container } from 'react-bootstrap';
 import PokemonItem from '../../components/pokemonItem/pokemonItem';
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 type PokemonDataItem = {
   name: string;
   src: string;
 };
-const pokemonData: PokemonDataItem[] = [
-  {
-    name: 'Bulbasaur',
-    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg',
-  },
-  {
-    name: 'Bulbasaur',
-    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg',
-  },
-  {
-    name: 'Venusaur',
-    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg',
-  },
-];
+
+interface IPokemonResults {
+  name: string;
+  url: string;
+}
+
+interface IPokemonData {
+  count: number;
+  next: string;
+  previous: string;
+  results: IPokemonResults[];
+}
 const Home = () => {
+  const [pokemonData, setpokemonData] = useState<PokemonDataItem[]>([]);
+
+  useEffect(() => {
+    api
+      .get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+      .then((response) => {
+        console.log(response);
+        const data: IPokemonData = response.data;
+        const results: PokemonDataItem[] = data?.results.map((item) => ({
+          name: item.name,
+          src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+        }));
+        setpokemonData(results);
+      });
+  });
   return (
-    <Container style={{ display: 'flex', gap: 10, marginTop: 10 }}>
+    <Container
+      style={{ display: 'flex', gap: 10, marginTop: 10, flexWrap: 'wrap' }}
+    >
       {pokemonData.map((item: PokemonDataItem) => {
-        return <PokemonItem src={item.src} name={item.name} />;
+        return <PokemonItem key={item.name} src={item.src} name={item.name} />;
       })}
     </Container>
   );
